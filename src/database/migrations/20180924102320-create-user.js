b1c56d44587cc8cae90fa089cac347133648bc78
Allan Jorge <contato.allanj@gmail.com>
2018-09-25 12:32:11 -0300

    database: add user model

---
+++ src/database/migrations/20180924102320-create-user.js
@@ -0,0 +1,33 @@
+const tableName = 'Users'
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable(tableName, {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    }),
+
+  down: queryInterface =>
+    queryInterface.dropTable(tableName),
+}
